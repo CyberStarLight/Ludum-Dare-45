65ffbd93239d0b45da387901082cef277f3e3d32
Cyber <moti.osmo+FreeStarLight@gmail.com>
2019-10-05 15:27:22 +0300

    Added 2 roads for prototype
    * Added dragon
    * added 2 roads
    * added followers
    * added logic for followers to find and walk along path
    * followers die when reaching dragon

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    public Road[] Roads;
+
+    public Transform TestSpawnPosition;
+    public RoadWalker TestWalkerPrefab;
+
+    private void Start()
+    {
+        //var newFollower = Instantiate(TestWalkerPrefab, TestSpawnPosition.position, Quaternion.identity, null);
+
+        //var searchResult = GetClosestPoint(newFollower.transform.position);
+
+        //newFollower.currentRoad = searchResult.ParentRoad;
+        //searchResult.ParentRoad.RegisterWalker(newFollower, searchResult.PointIndex);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var newFollower = Instantiate(TestWalkerPrefab, mousePos, Quaternion.identity, null);
+
+            var searchResult = GetClosestPoint(newFollower.transform.position);
+
+            newFollower.currentRoad = searchResult.ParentRoad;
+            searchResult.ParentRoad.RegisterWalker(newFollower, searchResult.PointIndex);
+        }
+    }
+
+    public Road.PointSearchResult GetClosestPoint(Vector3 position)
+    {
+        var closestPointOnEachRoad = Roads.Select(x => x.GetClosestPoint(position));
+        var closestRoadPoint = closestPointOnEachRoad.OrderBy(x => x.Distance).FirstOrDefault();
+
+        return closestRoadPoint;
+    }
+}
---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    public Transform[] points;
+
+    private HashSet<RoadWalker> registeredRoadWalkers;
+
+    private void Awake()
+    {
+        registeredRoadWalkers = new HashSet<RoadWalker>();
+
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Move all walkers registered to this path forward along this path.
+
+        foreach (var walker in registeredRoadWalkers)
+        {
+            if (walker.HasReachedRoadEnd)
+                continue;
+
+            //Get distance the walker needs to walk this frame
+            float distanceToTravel = walker.Speed * Time.fixedDeltaTime;
+            
+            while (distanceToTravel > 0f)
+            {
+                float distanceToNextPoint = Vector2.Distance(walker.transform.position, walker.currentTargetPosition);
+
+                //flip the sprite if needed
+                walker.MainRenderer.flipX = walker.currentTargetPosition.x < walker.transform.position.x;
+
+                if(distanceToNextPoint > distanceToTravel)
+                {
+                    //Move walker forward
+                    walker.transform.position = Vector2.MoveTowards(walker.transform.position, walker.currentTargetPosition, distanceToTravel);
+                    break;
+                }
+                else if (distanceToNextPoint <= distanceToTravel)
+                {
+                    //Move walker directly to the point, then continue moving towards the next point 
+                    walker.transform.position = walker.currentTargetPosition;
+                    distanceToTravel -= distanceToNextPoint;
+
+                    //set next target point, or stop walking if you reached the last point on the road
+                    if (walker.currentTargetPointIndex + 1 < points.Length)
+                    {
+                        walker.currentTargetPointIndex += 1;
+                        walker.currentTargetPosition = GetPositionFromPoint(walker.currentTargetPointIndex);
+                    }
+                    else
+                    {
+                        walker.currentTargetPointIndex = -1;
+                        walker.HasReachedRoadEnd = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void RegisterWalker(RoadWalker walker, int targetPointIndex)
+    {
+        walker.currentTargetPointIndex = targetPointIndex;
+        walker.currentTargetPosition = GetPositionFromPoint(targetPointIndex);
+
+        registeredRoadWalkers.Add(walker);
+    }
+
+    public void UnregisterWalker(RoadWalker walker)
+    {
+        registeredRoadWalkers.Remove(walker);
+    }
+
+    public Vector3 GetPositionFromPoint(int pointIndex)
+    {
+        if (pointIndex >= 0 && pointIndex < points.Length)
+            return points[pointIndex].position;
+        else
+            return Vector3.zero;
+    }
+
+    public PointSearchResult GetClosestPoint(Vector3 position)
+    {
+        var closestPoint = 
+            points
+            .Select(x => new { Point = x, Distance = Vector3.Distance(position, x.position) })
+            .OrderBy(x => x.Distance)
+            .First();
+        
+        return new PointSearchResult() {
+            ParentRoad = this,
+            PointObject = closestPoint.Point,
+            PointIndex = Array.IndexOf(points, closestPoint.Point),
+
+            OriginPosition = position,
+            PointPosition = closestPoint.Point.position,
+            Distance = closestPoint.Distance,
+        };
+    }
+    
+    public class Point
+    {
+        public Road ParentRoad;
+        public Transform PointObject;
+    }
+
+    public class PointSearchResult
+    {
+        public Road ParentRoad;
+        public Transform PointObject;
+        public int PointIndex;
+
+        public Vector3 OriginPosition;
+        public Vector3 PointPosition;
+        public float Distance;
+    }
+}
